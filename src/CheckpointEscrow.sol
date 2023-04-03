af1d6c8f16a97b66d7950d287d7faf11be20e053
steven nevins <steven.nevins.js@gmail.com>
2023-04-03 15:14:38 -0400

    escrow contract

---
+++ src/CheckpointEscrow.sol
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {ERC721Checkpointable, ERC721} from "src/CheckpointedERC721.sol";
+
+contract CheckpointEscrow {
+    address immutable token;
+    event Deposited(uint256 indexed blockNumber, uint256 weiAmount);
+    event Withdrawal(uint256 indexed blockNumber, uint256 weiAmount);
+
+    ///     blockNumber => amount
+    mapping(uint256 => uint256) private _deposits;
+    mapping(uint256 => bool) private _isClaimed;
+
+    constructor(address _token) {
+        token = _token;
+    }
+
+    function pendingFor(uint32 blockNumber, address payee)
+        public
+        view
+        returns (uint256)
+    {
+        uint256 balance = ERC721Checkpointable(token).getPastBalance(
+            payee,
+            blockNumber
+        );
+        uint256 supply = ERC721Checkpointable(token).getPastTotalSupply(
+            blockNumber
+        );
+        uint256 escrowedAmount = _deposits[blockNumber];
+
+        require(!_isClaimed[blockNumber], "already claimed");
+
+        return (escrowedAmount * balance) / supply;
+    }
+
+    function withdraw(uint32 blockNumber) public virtual {
+        uint256 amount = pendingFor(blockNumber, msg.sender);
+        emit Withdrawal(blockNumber, amount);
+    }
+
+    receive() external payable {
+        _deposits[block.number] = msg.value;
+    }
+}
