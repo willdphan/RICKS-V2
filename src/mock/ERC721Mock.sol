5996bc50f47b43fc31daf91cf340157efcfaf0a9
steven nevins <steven.nevins.js@gmail.com>
2023-04-05 08:10:21 -0400

    forge fmt

--- src/mock/ERC721Mock.sol
+++ src/mock/ERC721Mock.sol
@@ -5,11 +5,7 @@ pragma solidity ^0.8.0;
 import "lib/openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
 
 contract ERC721Mock is ERC721 {
-    
-    constructor(
-        string memory name,
-        string memory symbol
-    ) ERC721(name, symbol) {}
+    constructor(string memory name, string memory symbol) ERC721(name, symbol) {}
 
     function mint(uint256 tokenId) public {
         _safeMint(msg.sender, tokenId);
